6c22002e7ccea3d3c682db9a23d692ce35795eef
khoa-v-nguyen <khoa.nguyen@oddball.io>
2023-11-18 12:35:28 -0700

    refactored

--- futbol_league/stat_tracker.py
+++ futbol_league/stat_tracker.py
@@ -11,31 +11,12 @@ class CSVDataReader:
 
 class StatTracker:
     def __init__(self, file_path):
-        self.games = self.games_array(file_path['games'])
-        self.teams = self.teams_array(file_path['teams'])
-        self.game_teams = self.game_teams_array(file_path['game_teams'])
+        self.games = self.create_objects_from_csv(Game, file_path['games'])
+        self.teams = self.create_objects_from_csv(Team, file_path['teams'])
+        self.game_teams = self.create_objects_from_csv(GameTeam, file_path['game_teams'])
+        pdb.set_trace()
 
-    def games_array(self, games_path):
-        games_list = []
-        games_data = CSVDataReader.read_csv(games_path)
-        for index, game_row in games_data.iterrows():
-            game = Game(game_row)
-            games_list.append(game)
-        return games_list
-    
-    def teams_array(self, teams_path):
-        teams_list = []
-        teams_data = CSVDataReader.read_csv(teams_path)
-        for index, team_row in teams_data.iterrows():
-            team = Team(team_row)
-            teams_list.append(team)
-        return teams_list
-    
-    def game_teams_array(self, game_teams_path):
-        game_teams_list = []
-        game_teams_data = CSVDataReader.read_csv(game_teams_path)
-        for index, game_team_row in game_teams_data.iterrows():
-            game_team = GameTeam(game_team_row)
-            game_teams_list.append(game_team)
-        return game_teams_list
+    def create_objects_from_csv(self, cls, file_path):
+        data = CSVDataReader.read_csv(file_path)
+        return [cls(row) for _, row in data.iterrows()]
 
